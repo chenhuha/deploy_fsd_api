be5fe556a656b332768d7c002945095ea44ed3f7
chenhuha <zchlinux@163.com>
2023-04-21 15:52:44 +0800

    增加开始升级的解压

--- app/upgrade/upgrade.py
+++ app/upgrade/upgrade.py
@@ -1,16 +1,43 @@
 from flask_restful import reqparse, Resource
+from flask import jsonify
 import os
+import tarfile
 from flask import current_app
-from common import types
-from werkzeug.utils import secure_filename
+from threading import Thread
 
 class Upgrade(Resource):
-    
-    def _get_upload_from_request(self):
+
+    def _get_upgrade_from_request(self):
         parser = reqparse.RequestParser()
         parser.add_argument('filename', required=True, type=str, location='json',
                             help='The filename field does not exist')
         return parser.parse_args()
 
     def post(self):
-        os.path.join()
+        file_name = self._get_upgrade_from_request()['filename']
+        path = os.path.join(current_app.config['UPGRADE_SAVE_PATH'], file_name)
+        thread = Thread(target=self.decompression, args=(path))
+        thread.start()
+
+    def decompression(self,file_path):
+        try:
+            with open(current_app.config['DEPLOY_HOME'] + '/tmp/upgrade_now_status' , 'w') as f:
+                try:
+                    with tarfile.open(file_path) as tar:
+                        tar.extractall(current_app.config['UPGRADE_SAVE_PATH'])
+                        tar.close()
+                    data = self._data_build(True)
+                except:
+                    data = self._data_build(False)
+        except:
+            data = self._data_build(False)
+            f.write(jsonify([data]))
+    
+    def _data_build(self,ok):
+        return {
+            "en": "unzip_upgrade_package",
+            "message": "成功" if ok else "失败" ,
+            "result": ok,
+            "sort": 0,
+            "zh": "解压升级包成功" if ok else "解压升级包失败"
+        }
