a29df36488b86ce483a92433dd44ee010645a41f
caotingv <caotingv@qq.com>
2023-04-22 16:19:10 +0800

    fix net check api bug

--- app/deploy/net_check.py
+++ app/deploy/net_check.py
@@ -10,29 +10,33 @@ from flask_restful import reqparse, Resource
 
 
 class NetCheck(Resource, Node):
+    def __init__(self):
+        super().__init__()
+        self.nodes = self.get_nodes_from_request()
+        self.node_list = self.get_info_with_from(self.nodes)
+
     def post(self):
         data = {}
-        nodes = self.get_nodes_from_request()
 
-        if len(nodes) == 1:
-            data = self.single_node_data(nodes)
+        if len(self.nodes) == 1:
+            data = self.single_node_data()
         else:
-            data = self.multiple_nodes_data(nodes)
+            data = self.multiple_nodes_data()
 
-        node_info_file = os.path.join(self.deploy_home, "deploy_node_info.xlsx")
-        
-        if not os.path.isfile(node_info_file):
+        node_info_file = os.path.join(
+            self.deploy_home, "deploy_node_info.xlsx")
+        if os.path.isfile(node_info_file):
+            os.remove(node_info_file)
             source_file = os.path.join(self.template_path, "deployExcel.xlsx")
             shutil.copyfile(source_file, node_info_file)
         self.write_data_to_excel(node_info_file, data)
 
         return types.DataModel().model(code=0, data=data)
-    
-    # 单节点 
-    def single_node_data(self, nodes):
-        node_list = self.get_info_with_from(nodes)
+
+    # 单节点
+    def single_node_data(self):
         result = {}
-        for node in node_list:
+        for node in self.node_list:
             result = {
                 'sourceIp': node['management_ip'],
                 'sourceHostname': node['hostname'],
@@ -50,8 +54,7 @@ class NetCheck(Resource, Node):
         return self.combine_results(api_result, [], [])
 
     # 多节点
-    def multiple_nodes_data(self, nodes):
-        node_list = self.get_info_with_from(nodes)
+    def multiple_nodes_data(self):
         api_result = []
         ceph_cluster_result = []
         ceph_public_result = []
@@ -60,14 +63,14 @@ class NetCheck(Resource, Node):
             try:
                 output = self.iperf3_client(
                     current_node['management_ip'], node_ip, port)
-                return self.output_format_different_node(output, node_list)
+                return self.output_format_different_node(output)
             except Exception as e:
                 self._logger.error('get iperf3_client output failed, %s', e)
                 return self.output_format_null_node(current_node, node, type)
 
         # execute iperf3 client and collect results
-        for i, current_node in enumerate(node_list):
-            for j, node in enumerate(node_list):
+        for i, current_node in enumerate(self.node_list):
+            for j, node in enumerate(self.node_list):
                 if i == j:
                     api_result.append(
                         self.output_format_same_node(node, 'management'))
@@ -100,17 +103,17 @@ class NetCheck(Resource, Node):
                     node_info['management_ip'] = card['ip']
                     node_info['management_speed'] = card['speed']
                     node_info['management_mtu'] = card['mtu']
-    
+
                 if 'STORAGECLUSTER' in card['purpose']:
                     node_info['storage_cluster_ip'] = card['ip']
                     node_info['storage_cluster_speed'] = card['speed']
                     node_info['storage_cluster_mtu'] = card['mtu']
-   
+
                 if 'STORAGEPUBLIC' in card['purpose']:
                     node_info['storage_public_ip'] = card['ip']
                     node_info['storage_public_speed'] = card['speed']
                     node_info['storage_public_mtu'] = card['mtu']
-     
+
                 node_info['hostname'] = node['nodeName']
             node_list.append(node_info)
 
@@ -144,7 +147,8 @@ class NetCheck(Resource, Node):
         real_speed = int(speed) / 8
         mtu = node[f"{card_purpose}_mtu"]
         packet_loss_rate = '0%'
-        status = self._get_status(speed, real_speed, packet_loss_rate)
+        status = self._get_status(
+            speed, real_speed, packet_loss_rate, source_ip)
 
         result = {
             'sourceIp': source_ip,
@@ -161,21 +165,20 @@ class NetCheck(Resource, Node):
         return result
 
     # 处理 iperf 返回的数据
-    def output_format_different_node(self, result, node_list):
+    def output_format_different_node(self, result):
         json_data = json.loads(result)
 
         local_host = json_data['start']['connected'][0]['local_host']
         remote_host = json_data['start']['connected'][0]['remote_host']
         bits_per_second = json_data['end']['sum_received']['bits_per_second']
 
-        source_hostname = self._get_hostname(local_host, node_list)
-        dest_hostname = self._get_hostname(remote_host, node_list)
-        speed = self._get_speed(local_host, node_list)
+        source_hostname = self._get_hostname(local_host)
+        dest_hostname = self._get_hostname(remote_host)
+        speed = self._get_speed(local_host)
         real_speed = self._get_realSpeed(bits_per_second)
-        mtu = self._get_mtu(local_host, node_list)
+        mtu = self._get_mtu(local_host)
         plr = self._get_packet_loss_rate(local_host, remote_host)
-        status = self._get_status(
-            speed, real_speed, plr, local_host, node_list)
+        status = self._get_status(speed, real_speed, plr, local_host)
 
         result = {
             'sourceIp': local_host,
@@ -190,7 +193,7 @@ class NetCheck(Resource, Node):
         }
 
         return result
-    
+
     def output_format_null_node(self, current_node, node, card_purpose):
         source_ip = current_node[f"{card_purpose}_ip"]
         source_hostname = current_node["hostname"]
@@ -235,8 +238,8 @@ class NetCheck(Resource, Node):
 
         return packet_loss
 
-    def _get_hostname(self, node_ip, node_list):
-        for node in node_list:
+    def _get_hostname(self, node_ip):
+        for node in self.node_list:
             for key in ['management_ip', 'storage_cluster_ip', 'storage_public_ip']:
                 if node[key] == node_ip:
                     return node['hostname']
@@ -245,36 +248,38 @@ class NetCheck(Resource, Node):
     def _get_realSpeed(self, bits_per_second):
         return round(bits_per_second / 1000000 / 8, 2)
 
-    def _get_speed(self, node_ip, node_list):
-        for node in node_list:
+    def _get_speed(self, node_ip):
+        for node in self.node_list:
             for key in ['management_ip', 'storage_cluster_ip', 'storage_public_ip']:
                 if node[key] == node_ip:
                     return node[key.replace('ip', 'speed')]
         return None
 
-    def _get_mtu(self, node_ip, node_list):
-        for node in node_list:
+    def _get_mtu(self, node_ip):
+        for node in self.node_list:
             for key in ['management_ip', 'storage_cluster_ip', 'storage_public_ip']:
                 if node[key] == node_ip:
                     return node[key.replace('ip', 'mtu')]
         return None
 
-    def _get_status(self, speed, real_speed, plr, node_ip='', node_list=[]):
+    def _get_status(self, speed, real_speed, plr, node_ip=''):
         if plr != '0%':
             self._logger.info('plr not is 0%')
             return 1
 
-        for node in node_list:
+        for node in self.node_list:
             if node['management_ip'] == node_ip:
                 if int(node['management_speed']) < 1000:
                     self._logger.info(
                         'The speed of the management network card is less than 1000')
                     return 2
+
             if node['storage_cluster_ip'] == node_ip:
                 if int(node['storage_cluster_speed']) < 10000:
                     self._logger.info(
                         'The speed of the storage cluster network card is less than 10000')
                     return 2
+
             if node['storage_public_ip'] == node_ip:
                 if int(node['storage_public_speed']) < 10000:
                     self._logger.info(
@@ -293,42 +298,48 @@ class NetCheck(Resource, Node):
         workbook = openpyxl.load_workbook(filepath)
         worksheet = workbook.active
         start_num = 3
-        
+
         status_map = {0: '正常', 1: '异常', 2: '警告'}
         card_name_map = {'apiResult': '管理网',
-                        'cephPublicResult': '存储公网',
-                        'cephClusterResult': '存储集群网'}
+                         'cephPublicResult': '存储公网',
+                         'cephClusterResult': '存储集群网'}
         for card_name, card_data in data.items():
             card_title = card_name_map.get(card_name, card_name)
             for item in card_data:
                 worksheet.cell(row=start_num, column=1, value=card_title)
                 worksheet.cell(row=start_num, column=2, value=item['sourceIp'])
                 worksheet.cell(row=start_num, column=3, value=item['destIp'])
-                worksheet.cell(row=start_num, column=4, value=status_map.get(item['status'], ''))
+                worksheet.cell(row=start_num, column=4,
+                               value=status_map.get(item['status'], ''))
                 worksheet.cell(row=start_num, column=5, value=str(item))
                 start_num += 1
         workbook.save(filepath)
 
 
 class NetCheckCommon(NetCheck):
-    def post(self):
-        data = {}
+    def __init__(self):
         nodes = self.get_nodes_from_request()
         cards = self.get_cards_from_request()
-        nodes = self.uniform_format_with_nodes(nodes, cards)
+        self.nodes = self.uniform_format_with_nodes(nodes, cards)
+        self.node_list = self.get_info_with_from(self.nodes)
+        super().__init__()
 
-        if len(nodes) == 1:
-            data = self.single_node_data(nodes)
+    def post(self):
+        data = {}
+
+        if len(self.nodes) == 1:
+            data = self.single_node_data()
         else:
-            data = self.multiple_nodes_data(nodes)
+            data = self.multiple_nodes_data()
 
-        node_info_file = os.path.join(self.deploy_home, "deploy_node_info.xlsx")
-        
-        if not os.path.isfile(node_info_file):
+        node_info_file = os.path.join(
+            self.deploy_home, "deploy_node_info.xlsx")
+        if os.path.isfile(node_info_file):
+            os.remove(node_info_file)
             source_file = os.path.join(self.template_path, "deployExcel.xlsx")
             shutil.copyfile(source_file, node_info_file)
         self.write_data_to_excel(node_info_file, data)
-        
+
         return types.DataModel().model(code=0, data=data)
 
     def get_cards_from_request(self):
